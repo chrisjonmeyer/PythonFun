a0a9b0a0dbd63f9e6d04b9938b35182e85ba5412
Meyer <christopher.meyer@igt.com>
2019-11-14 14:43:07 -0500

    FizzBuzz question

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+number = 0
+
+while number < 100:
+    number += 1
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else: 
+        print(number)
