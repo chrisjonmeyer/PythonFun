bba85523695682ee84799e363427e5b8d6675cec
Meyer <christopher.meyer@igt.com>
2019-11-23 14:00:30 -0500

    OO testing

---
+++ ObjectOrientedTesting.py
@@ -0,0 +1,14 @@
+# Object Oriented Homework From Bootcamp
+# Use https://github.com/Pierian-Data/Complete-Python-3-Bootcamp
+
+# Create a class with methods to accept coordinates as a pair of tuples and return the slope and distance of the line.
+
+class Line:
+
+    def __init__(self,coor1,coor2):
+        self.coor1 = coor1
+        self.coor2 = coor2
+    
+    def distance(self):
+        
+    def slope(self):
