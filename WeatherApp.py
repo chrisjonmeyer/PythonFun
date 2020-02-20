9e350a5aee9beb36c417643408a29171b8b1d820
Meyer <christopher.meyer@igt.com>
2020-02-20 16:45:37 -0500

    Some edits

--- WeatherApp.py
+++ WeatherApp.py
@@ -6,13 +6,21 @@
 import json
 import socket
 import re
+from pprint import pprint
+import requests
+import geoip2.webservice
 
 def get_my_ip():
     try:
         host_name = socket.gethostname()
         ip_address = socket.gethostbyname(host_name)
         print("My IP Address is " + ip_address)
+        print("My hostname is " + host_name)
+        return ip_address
     except:
         print("Unable to get IP Address")
 
-get_my_ip()
+my_ip = get_my_ip()
+
+my_request = requests.get('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=7abc9a643ec0a91b1e604ce98f51501c')
+print (my_request.json())
