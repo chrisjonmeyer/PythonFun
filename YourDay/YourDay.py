45ab6cbdfa923a851576c34363cb299360cb8505
Chris Meyer <cmeyerbusiness@gmail.com>
2020-08-03 15:56:34 -0400

    Creating a new function

---
+++ YourDay/YourDay.py
@@ -0,0 +1,8 @@
+# Creating a script that gets your day going via email.
+# Sends Weather and Calendar Meetings
+
+# Create: Function - get weather
+
+# Create: Function - get schedule
+
+# Create Function - send email
